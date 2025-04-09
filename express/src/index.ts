704630540d41549b917da3683a1761ee1a7baf06
kento <na.ken0627@gmail.com>
2025-04-09 13:15:44 +0900

    make conection between backend andfrontend

--- express/src/index.ts
+++ express/src/index.ts
@@ -1,16 +1,32 @@
 import express from "express";
-import * as dotenv from "dotenv";
+import * as dotenv from "dotenv"; //環境変数が設定可能
+import cors from "cors";
 
 import type { Express, Request, Response } from "express";
 
-dotenv.config();
+import apiRouter from "./routes/api";
 
 const app: Express = express();
+
+//環境変数を設定するdotenv(未設定)
+dotenv.config();
 const port = process.env.PORT || 3001;
 
+//特定のサーバからのアクセスを許可するcors設定
+const corsOptions = {
+  origin: "http://localhost:3000",
+};
+app.use(cors(corsOptions));
+
+//apiにリクエストが来た際の処理
+app.use("/api", apiRouter);
+
+//ルートパスにリクエストがきた際の処理
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.send("Hello World!!");
 });
+
+//サーバを起動
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
