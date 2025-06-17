4f28b27b76ed4149422eba126b33cb8c44d0b7ec
kento <na.ken0627@gmail.com>
2025-06-18 08:47:22 +0900

    fix:split testcase because of  multiple assartions

--- next.js/src/__tests__/UserCalcProductAmount.test.tsx
+++ next.js/src/__tests__/UserCalcProductAmount.test.tsx
@@ -4,22 +4,26 @@ import { CartProduct } from "@/types/cart";
 import { mockProducts } from "./mocks/mockProducts";
 
 describe("Test UserCalcProductTotalAmount", () => {
+  test("カートの商品単位の小計算出(標準品)", () => {
+    const cartProducts: CartProduct[] = [
+      { productId: 1, customizationId: null, quantity: 1 },
+      { productId: 1, customizationId: 101, quantity: 2 },
+    ];
+    const result = calcProductTotalAmount(mockProducts, cartProducts, 1, null);
+    expect(result).toBe(100);
+  });
+
   test("カートの商品単位の小計算出(標準品＆カスタム品)", () => {
     const cartProducts: CartProduct[] = [
       { productId: 1, customizationId: null, quantity: 1 },
       { productId: 1, customizationId: 101, quantity: 2 },
     ];
-
-    const result1 = calcProductTotalAmount(mockProducts, cartProducts, 1, null);
-    expect(result1).toBe(100);
-
-    const result2 = calcProductTotalAmount(mockProducts, cartProducts, 1, 101);
-    expect(result2).toBe(220);
+    const result = calcProductTotalAmount(mockProducts, cartProducts, 1, 101);
+    expect(result).toBe(220);
   });
 
   test("カートが空の場合の金額計算", () => {
     const cartProducts: CartProduct[] = [];
-
     const result = calcProductTotalAmount(mockProducts, cartProducts, 1, null);
     expect(result).toBe(0);
   });
